590fce1fc220e132e9d2042a96c2462187ac3870
mucahittanacioglu <mucahittanacioglu@gmail.com>
2023-08-27 18:28:57 +0300

    add post

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,36 @@ import CustomNav from './component/CustomNav'
 import {AiFillDashboard, AiOutlineNotification, AiOutlineUser} from "react-icons/ai"
 import {BsSearch} from "react-icons/bs";
 import {BiMessageAlt} from "react-icons/bi";
-
+import Post from "./component/post/Post.jsx";
+import 'react-bootstrap/dist/react-bootstrap.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Col, Container, Row} from "react-bootstrap";
 
 function App() {
-  return (
-    <>
-      <CustomNav
-      li={[
-        ["Dashboard", <AiFillDashboard/>],
-        ["Profile", <AiOutlineUser/> ],
-        ["Messages", <BiMessageAlt/> ],
-        ["Notifications", <AiOutlineNotification/> ],
-        ["Search", <BsSearch/> ]
-      ]}
-    />
+    return (
+        <>
 
-    </>
-  )
+            <Container>
+                <Row>
+                    <Col>
+                        <CustomNav
+                            li={[
+                                ["Dashboard", <AiFillDashboard/>],
+                                ["Profile", <AiOutlineUser/>],
+                                ["Messages", <BiMessageAlt/>],
+                                ["Notifications", <AiOutlineNotification/>],
+                                ["Search", <BsSearch/>]
+                            ]}
+                        />
+                    </Col>
+                    <Col>
+                        <Post/>
+                    </Col>
+                </Row>
+            </Container>
+
+        </>
+    )
 }
 
 export default App
---
+++ src/component/post/post-components/header/PostHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostHeader = () => {
+    return (
+        <div>
+            <h1> Test Header</h1>
+        </div>
+    );
+};
+
+export default PostHeader;
