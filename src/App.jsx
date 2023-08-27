8f8a0169bcf3bc53d626d3af7215de206747faeb
emre <emrecelikfw@gmail.com>
2023-08-27 17:28:23 +0300

    added navigation icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import CustomNav from './component/CustomNav'
+import {AiFillDashboard, AiOutlineNotification, AiOutlineUser} from "react-icons/ai"
+import {BsSearch} from "react-icons/bs";
+import {BiMessageAlt} from "react-icons/bi";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CustomNav
+      li={[
+        ["Dashboard", <AiFillDashboard/>],
+        ["Profile", <AiOutlineUser/> ],
+        ["Messages", <BiMessageAlt/> ],
+        ["Notifications", <AiOutlineNotification/> ],
+        ["Search", <BsSearch/> ]
+      ]}
+    />
+
     </>
   )
 }
---
+++ src/component/CustomNav.jsx
@@ -0,0 +1,42 @@
+import  { useState } from "react";
+import './_CustomNav.scss';
+import {CiMenuBurger} from "react-icons/ci"
+
+const CustomNav = ({ li }) => {
+  const [window, setWindow] = useState(false);
+
+  let openClose = () => {
+    if (window === false) {
+      setWindow(true);
+    } else {
+      setWindow(false);
+    }
+  };
+  return (
+    <nav className="navbar-menu" style={{ width: window === false ? 250 : 60 }}>
+      <div className="burger" onClick={() => openClose()}>
+        <span className="burgerIcon"><CiMenuBurger/></span>
+      </div>
+      <ul className="navbar__list">
+        {li.map((item, i) => (
+          <div className="navbar__li-box" key={i}>
+            {/* <img
+              src={item[1]}
+              alt={item[1]}
+              style={{ paddingLeft: window === false ? 27 : 17 }}
+            /> */}
+            {item[1]}
+            <li
+              className="navbar__li"
+              style={{ display: window === false ? "inline-block" : "none" }}
+            >
+              {item[0]}
+            </li>
+          </div>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default CustomNav;
