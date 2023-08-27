0c49c50a1f7793ab908aca0e79e1f318a6f46ccf
mucahittanacioglu <mucahittanacioglu@gmail.com>
2023-08-27 18:11:41 +0300

    add post

---
+++ src/component/post/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostHeader from "./post-components/header/PostHeader.jsx";
+
+const Post = () => {
+    return (
+        <div>
+            <PostHeader/>
+        </div>
+    );
+};
+
+export default Post;
